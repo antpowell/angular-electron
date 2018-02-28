f2954128d6846dd06125bc289e084d5f7a6b3960
antpowell <powell.anthony.e@gmail.com>
2018-02-28 01:00:05 -0600

    added core angular svg round progressbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/takeWhile';
+import 'rxjs/add/operator/do';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  max = 1;
+  current = 0;
   title = 'app';
+
+  start() {
+    const interval = Observable.interval(100);
+
+    interval.takeWhile(_ => !this.isFinshed)
+    .do(i => this.current += 0.1)
+    .subscribe();
+  }
+
+  finish() {
+    this.current = this.max;
+  }
+
+  reset() {
+    this.current = 0;
+  }
+
+  get maxVal(){
+    return isNaN(this.max) || this.max < 0.1 ? 0.1 : this.max;
+  }
+
+  get currentVal(){
+    return isNaN(this.current) || this.current < 0.1 ? 0.1 : this.current;
+  }
+  get isFinshed(){
+    return this.currentVal >= this.maxVal;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {
+  BrowserModule
+} from '@angular/platform-browser';
+import {
+  NgModule
+} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {
+  AppComponent
+} from './app.component';
+import {
+  FormsModule
+} from '@angular/forms';
+import {
+  RoundProgressModule
+} from 'angular-svg-round-progressbar';
+
+
 
 
 @NgModule({
@@ -10,9 +24,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RoundProgressModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
